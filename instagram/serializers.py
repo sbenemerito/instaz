3495fcfe554cb8657b2040e77c508a6bbc722c13
Sam Benemerito <benemeritosam@gmail.com>
2019-06-22 15:30:18 +0900

    include Like details in Post serializer

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -27,7 +27,18 @@ class PostSerializer(serializers.ModelSerializer):
 
     tags = serializers.StringRelatedField(many=True)
     author = UserSerializer()
+    likes = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
+
+    def get_likes(self, obj):
+        return obj.likes
+
+    def get_is_liked(self, obj):
+        if 'request' in self.context:
+            return obj.is_liked_by(self.context['request'].user)
+
+        return False
