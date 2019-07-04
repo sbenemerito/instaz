8441a3961e7b99094022131bb923583d35a0639a
Sam Benemerito <benemeritosam@gmail.com>
2019-07-04 23:11:26 +0900

    add filler Login and Signup components

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Login extends React.Component {
+
+  render() {
+    return (
+      <div className="Login">
+        <div className="ui center aligned">
+          <h1>Login</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Signup extends React.Component {
+
+  render() {
+    return (
+      <div className="Signup">
+        <div className="ui center aligned">
+          <h1>Signup</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
