847b9e14e13e02163eb9f587dcb990a3d9dcf42f
Sam Benemerito <benemeritosam@gmail.com>
2019-07-10 23:15:49 +0900

    initial PostDetail component

---
+++ frontend/src/components/PostDetail.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import PostListItem from './PostListItem';
+import { likePost, viewPost } from '../actions';
+
+class PostDetail extends React.Component {
+  componentDidMount() {
+    this.props.viewPost(this.props.match.params.id);
+  }
+
+  render() {
+    const loadingDOM = (
+      <div>
+        <h1>Loading!</h1>
+      </div>
+    );
+    const postDOM = post => (
+      <div className="ui two column stackable grid">
+        <div className="left aligned nine wide computer sixteen wide mobile column">
+          <PostListItem
+            key={post.id}
+            post={post}
+            likePost={this.props.likePost}
+            showActions={this.props.currentUser ? true : false}
+            isPreview={false} />
+        </div>
+        <div className="six wide column computer only">
+          <h1>Side Menu</h1>
+        </div>
+      </div>
+    );
+
+    return (
+      <div className="Home">
+        <div className="ui container center aligned">
+          {
+            this.props.currentPost
+            ? postDOM(this.props.currentPost)
+            : loadingDOM
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const { currentPost, currentUser } = state;
+  return { currentPost, currentUser };
+}
+
+export default connect(
+  mapStateToProps,
+  { likePost, viewPost }
+)(PostDetail);
